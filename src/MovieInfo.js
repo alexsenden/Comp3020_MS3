cd6507394e39fa17b7ffeb0c1f7101ee1de79293
alexsenden <alexandersenden@gmail.com>
2022-11-09 23:36:18 -0600

    Add collection class for lists of small icons, add X button to small icons

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,9 +1,22 @@
 import HappyGilmoreCover from './HappyGilmoreCover.jpg'
+import AlienCover from './AlienCover.jpg'
+import MinionsCover from './MinionsCover.jpg'
 
 export const movieInfo = {
+    Alien: {
+        title: "Alien",
+        coverRoute: AlienCover,
+        genre: "Horror",
+    },
     HappyGilmore: {
         title: "Happy Gilmore",
         coverRoute: HappyGilmoreCover,
-        genre: "",
+        genre: "Comedy",
+    },
+    MinionsRiseOfGru: {
+        title: "Minions: Rise of Gru",
+        coverRoute: MinionsCover,
+        genre: "Comedy",
     }
+
 };
--- src/index.js
+++ src/index.js
@@ -2,22 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { movieInfo } from './MovieInfo';
+import xButtonIcon from './X.png'
 
 class SmallIcon extends React.Component {
     render() {
         return (
             <div className="icon-block">
-                <img className="icon" src={movieInfo.HappyGilmore.coverRoute} alt="Happy Gilmore Cover"/>
-                <div className="icon-text">{movieInfo.HappyGilmore.title}</div>
+                <img className="icon" src={this.props.movie.coverRoute} alt="Happy Gilmore Cover"/>
+                <div className="icon-text">{this.props.movie.title}</div>
+                <input className="small-x-button" type="image" src={xButtonIcon} alt="X"></input>
+            </div>
+        );
+    }
+}
+
+class SmallIconCollection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movieList: [movieInfo.Alien, movieInfo.HappyGilmore, movieInfo.MinionsRiseOfGru, movieInfo.Alien, movieInfo.HappyGilmore, movieInfo.MinionsRiseOfGru, movieInfo.Alien, movieInfo.HappyGilmore, movieInfo.MinionsRiseOfGru],
+        };
+    }
+
+    addItem(item) {
+        let newState = this.state.movieList.slice();
+        newState.push(item);
+        this.setState({movieList: item});
+    }
+
+    render() {
+        return (
+            <div className="small-icon-collection">
+                {this.state.movieList.map(function(m, idx) {
+                    return (
+                        <li key={idx}>
+                            <SmallIcon movie={m} />
+                        </li>
+                    )
+                })}
             </div>
         );
     }
 }
 
 
-
-
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SmallIcon />);
+root.render(<SmallIconCollection />);
